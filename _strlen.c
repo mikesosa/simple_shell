2b950cc28867bda861b0baed68b6808077061d49
Daniel E Rodriguez <dr2d4@live.com>
2019-08-21 14:54:55 -0500

    strlen function

---
+++ _strlen.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * _strlen - lenght of a string
+ * @s: string to count
+ * 
+ * Return: lenght of a string
+ */
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen(s + 1));
+}
+
