430f76ee56d99e9688e0add7588eadf8981cdb55
Daniel E Rodriguez <dr2d4@live.com>
2019-08-21 14:52:55 -0500

    auxiliary functions shell

---
+++ _strdup.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+
+/**
+ * *_strdup - copies an string to an array
+ * @s: Source string
+ * 
+ * Return: @dest which is the new memory location.
+ */
+char *_strdup(char *s)
+{
+	char *dest;
+	int i = 0;
+
+	if (s == NULL)
+		return (NULL);
+
+	while (s[i])
+		i++;
+
+	dest = malloc(sizeof(char) * i + 1);
+
+	if (dest == NULL)
+		return (NULL);
+
+	i = 0;
+
+	for (i = 0; s[i]; i++)
+		dest[i] = s[i];
+
+	dest[i] = s[i];
+	return (dest);
+}
---
+++ free_args.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * free_args - function that frees memory allocations
+ */
+void free_args(void)
+{
+	int i = 0;
+
+	if (!shell.tty)
+		i = 1;
+
+	for (; shell.argv[i]; i++)
+		free(shell.argv[i]);
+}
+
