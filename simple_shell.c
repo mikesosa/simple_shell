6b414da755c7a07ca93d4a52e078523521978813
Daniel E Rodriguez <dr2d4@live.com>
2019-08-28 02:09:57 -0500

    test 0

--- simple_shell.c
+++ simple_shell.c
@@ -144,7 +144,7 @@ int exec_command(shell_t *shell)
 void print_prompt_tty(shell_t *shell)
 {
 	if (!shell || (shell && (shell->tty == 3)))
-		_puts("\x1B[1;33m#shellyta$\x1B[0m "); /*Prints the promt in color*/
+		_puts("#cisfun$ "); /*Prints the promt in color*/
 
 	fflush(stdout);
 }
